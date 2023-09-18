2f63aa332ee9b8e007998cb18d2975832085f0a1
Thiago Torres Costa <thiago.t0rres1@hotmail.com>
2023-09-18 13:57:47 -0300

    Components

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,12 @@
 import {Container} from "./styles.js"
+import { Button } from "../../components/Button/index.jsx"
 
 export function Details() {
   
   return(
     <Container>
       <h1>Hello World!</h1>
+      <Button></Button>
     </Container>
   )
 }
