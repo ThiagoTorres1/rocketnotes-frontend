c5fd8d7ddbe9b511247a6c028ab983ed1d4ae29d
Thiago Torres Costa <thiago.t0rres1@hotmail.com>
2023-09-19 09:39:48 -0300

    adding button text

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,6 +1,7 @@
 import {Container, Links} from "./styles.js"
 import { Header } from "../../components/Header"
 import { Section } from "../../components/Section"
+import { ButtonText } from "../../components/ButtonText"
 import { Button } from "../../components/Button"
 import { Tag } from "../../components/Tags"
 
@@ -9,6 +10,7 @@ export function Details() {
   return(
     <Container>
       <Header></Header>
+      <ButtonText title="Excluir Nota"/>
       <Section title="Links úteis">
         <Links>
           <li><a href="#">https://www.rocketseat.com.br/</a></li>
