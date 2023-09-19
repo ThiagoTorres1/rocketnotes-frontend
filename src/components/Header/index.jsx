c1d65d4be94b848d806b88c3d1c20912b7d4f6c4
Thiago Torres Costa <thiago.t0rres1@hotmail.com>
2023-09-19 08:28:43 -0300

    user profile

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,18 @@
-import { Container } from "./styles";
+import { Container, Profile } from "./styles";
 
 export function Header() {
   return(
     <Container>
-      
+      <Profile>
+        <img 
+          src="https://github.com/thiagotorres1.png" 
+          alt="Foto do usuário" 
+        />
+        <div>
+          <span>Bem-vindo</span>
+          <strong>Thiago Torres</strong>
+        </div>
+      </Profile>
     </Container>
   )
 }
