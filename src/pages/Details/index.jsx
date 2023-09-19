7889728528dd22b0ed0e76dacc9f16a4c58d5987
Thiago Torres Costa <thiago.t0rres1@hotmail.com>
2023-09-19 09:16:50 -0300

    children

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,5 +1,6 @@
-import {Container} from "./styles.js"
+import {Container, Links} from "./styles.js"
 import { Header } from "../../components/Header"
+import { Section } from "../../components/Section"
 import { Button } from "../../components/Button"
 
 export function Details() {
@@ -7,6 +8,15 @@ export function Details() {
   return(
     <Container>
       <Header></Header>
+      <Section title="Links úteis">
+        <Links>
+          <li><a href="#">https://www.rocketseat.com.br/</a></li>
+          <li><a href="#">https://www.rocketseat.com.br/</a></li>          
+        </Links>
+      </Section>
+      <Section title="Marcadores">
+
+      </Section>
       <Button title="Voltar"/>
     </Container>
   )
