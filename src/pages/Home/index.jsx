1951f89f7d040d1a2c2ee1ff2f6648ecb1e67d7d
Thiago Torres Costa <thiago.t0rres1@hotmail.com>
2023-09-20 08:05:18 -0300

    adding input component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,7 @@
+import { FiPlus, FiSearch } from 'react-icons/fi'
 import {Container, Brand, Menu, Search, Content, NewNote} from './styles'
 import {Header} from '../../components/Header'
+import {Input} from '../../components/Input'
 import {ButtonText} from '../../components/ButtonText'
  
 
@@ -15,9 +17,14 @@ export function Home() {
         <li><ButtonText title="React" /></li>
         <li><ButtonText title="NodeJs" /></li>
       </Menu>
-      <Search></Search>
+      <Search>
+        <Input placeholder="Pesquisar pelo título" icon={FiSearch}/>
+      </Search>
       <Content></Content>
-      <NewNote></NewNote>
+      <NewNote>
+        <FiPlus/>
+        Criar nota
+      </NewNote>
     </Container>
   )
 }
