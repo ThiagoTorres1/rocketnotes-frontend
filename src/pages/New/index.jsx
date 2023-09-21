05d20337c797f15e5974f142bef2b66e5e21b495
Thiago Torres Costa <thiago.t0rres1@hotmail.com>
2023-09-21 08:32:50 -0300

    adding page new

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -2,6 +2,9 @@ import {Container, Form} from './styles'
 import {Header} from '../../components/Header'
 import {Input} from '../../components/Input'
 import {Textarea} from '../../components/Textarea'
+import {NoteItem} from '../../components/NoteItem'
+import {Section} from '../../components/Section'
+import {Button} from '../../components/Button'
 
 export function New() {
   return(
@@ -15,6 +18,17 @@ export function New() {
           </header>
           <Input placeholder="Título"/>
           <Textarea placeholder="Observações"/>
+          <Section title="Links Úteis">
+            <NoteItem value="https://rocketseat.com.br"/>
+            <NoteItem isNew placeholder="Novo Link"/>
+          </Section>
+          <Section title="Marcadores">
+            <div className='tags'>
+              <NoteItem value="React"/>
+              <NoteItem isNew placeholder="Nova Tag"/>
+            </div>
+          </Section>
+          <Button title="Salvar" />
         </Form>
       </main>
     </Container>
