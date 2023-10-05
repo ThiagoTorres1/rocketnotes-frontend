22d6de9a31f5a2b9d4f0b90715b8228ce7ac6d43
Thiago Torres Costa <thiago.t0rres1@hotmail.com>
2023-10-05 14:59:32 -0300

    signIn and signOut

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { useAuth } from "../../hooks/auth";
 import { Container, Form, Background } from "./styles";
 import { Link } from "react-router-dom";
 import {FiMail, FiLock} from 'react-icons/fi'
@@ -5,6 +7,15 @@ import {Input} from '../../components/Input'
 import {Button} from '../../components/Button'
 
 export function SignIn() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const { signIn } = useAuth()
+  
+  function handleSignIn() {
+    signIn({email, password})
+  }
+
   return(
     <Container>
       <Form>
@@ -16,15 +27,17 @@ export function SignIn() {
           placeholder="E-mail"
           type="text"
           icon={FiMail}
+          onChange={e => setEmail(e.target.value)}
         />
 
         <Input 
           placeholder="Senha"
           type="password"
           icon={FiLock}
+          onChange={e => setPassword(e.target.value)}
         />
 
-        <Button title="Entrar"/>
+        <Button title="Entrar" onClick={handleSignIn}/>
         <Link to="/register">Criar conta</Link>
       </Form>
       <Background/>
