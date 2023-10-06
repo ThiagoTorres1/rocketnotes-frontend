4470c792f580fd562d45da4c31b2bfe382d02180
Thiago Torres Costa <thiago.t0rres1@hotmail.com>
2023-10-06 07:55:31 -0300

    header atualization

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -10,7 +10,7 @@ function AuthProvider({children}) {
     try {
       const response = await api.post("/sessions", {email, password})
       const {user, token} = response.data
-      api.defaults.headers.authorization = `Bearer ${token}`
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`
       setData({user, token})
 
       localStorage.setItem("@rocketnotes:user", JSON.stringify(user))
@@ -37,7 +37,7 @@ function AuthProvider({children}) {
     const user = localStorage.getItem("@rocketnotes:user")
 
     if(token && user) {
-      api.defaults.headers.authorization = `Bearer ${token}`
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`
       setData({
         token,
         user: JSON.parse(user)
